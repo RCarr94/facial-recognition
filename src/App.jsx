76eb26fa2b78f75d7201ce27ad88ad2c6b4153e3
Ryan Carr <ryan.g.carr@hotmail.com>
2023-09-29 17:12:33 -0400

    added particles bg pks

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import './App.css';
-
+import ParticlesBg from 'particles-bg';
 // Components
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 
 export default function App() {
   return (
     <div className="App">
+      <ParticlesBg type="cobweb" bg={true} />
       <Navigation />
       <Logo />
-      {/* <ImageLinkForm />
-      <FacialRecognition /> */}
+      <Rank />
+      <ImageLinkForm />
+      {/* <FacialRecognition /> */}
     </div>
   );
 }
---
+++ src/components/Rank/Rank.jsx
@@ -0,0 +1,14 @@
+
+
+export default function Rank() {
+  return (
+    <div>
+      <div className="white f3">
+        {'This will be dynamic'}
+      </div>
+      <div className="white f1">
+        {'#1'}
+      </div>
+    </div>
+  );
+}
